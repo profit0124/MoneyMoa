c2ac21fbdf2264d77ed9ae42a9bb2695528bb8b5
profit <profit0124@gmail.com>
2025-07-25 14:31:11 +0900

    feat: initial iOS project setup with gitignore

---
+++ MoneyMoa/MoneyMoaApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoneyMoaApp.swift
+//  MoneyMoa
+//
+//  Created by Sooik Kim on 7/25/25.
+//
+
+import SwiftUI
+
+@main
+struct MoneyMoaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MoneyMoaTests/MoneyMoaTests.swift
@@ -0,0 +1,17 @@
+//
+//  MoneyMoaTests.swift
+//  MoneyMoaTests
+//
+//  Created by Sooik Kim on 7/25/25.
+//
+
+import Testing
+@testable import MoneyMoa
+
+struct MoneyMoaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
